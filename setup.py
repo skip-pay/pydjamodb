a4a726e0c80987af704245213dceab8c4075c79d
Petr Olah <donthave@email.cz>
2023-01-17 00:49:01 +0100

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	tests/requirements.txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skip-pydjamodb',
-    version='0.0.9.1',
+    version='0.0.9.2',
     description="Django interface to PyDjamoDB.",
     keywords='django, DynamoDB, PyDjamoDB',
     author='Lubos Matl',
