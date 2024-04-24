d695b1b8977f5dcc0f053fbee75935651167d229
Tomáš Daniel <tomas.daniel@skippay.cz>
2024-04-24 09:09:03 +0200

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skip-pydjamodb',
-    version='0.1.0',
+    version='0.1.1',
     description="Django interface to PyDjamoDB.",
     keywords='django, DynamoDB, PyDjamoDB',
     author='Lubos Matl',
